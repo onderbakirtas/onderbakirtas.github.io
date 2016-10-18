8608e21003768b60d6ab7948ec184b50e39c97f1
Önder Emre <onderbakirtas@gmail.com>
2016-10-18 17:10:25 +0300

    Blog

---
+++ projeler/blog/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Onder's Digital Handwriting</title>
+    <link href="https://fonts.googleapis.com/css?family=Ek+Mukta:300,600|Poppins:300,700|Cookie&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1 class="author">Onder's Keylog</h1>
+        </div>
+        <div class="post-header">
+            <span class="date-box">26.09<br>2016</span>
+            <span class="post-title"><h4>Top 30 Best Free Blogger Templates To Create A Beautiful Blog Or Magazine Website 2016</h4></span>
+            <span class="comment-box">23</span>
+        </div>
+        <div class="post-content">
+            <figure>
+                <img src="img/thumbnail.jpg">
+                <figcaption>Some Random Text About The Photo</figcaption>
+            </figure>
+            <article>
+                <p>Blogger is possibly the oldest blogging platform still actively running on the web, Google Blogger has been home to millions of bloggers for over 16 years now, and still maintains a quite stable market share in the content management platform
+                    list. Out of all the known websites that run a content management system, Blogger is used by 2.8% of those websites, which comes out at being used on 1.2% of all known websites to man; thanks W3Techs. Some stick with Blogger because
+                    it’s made by Google, and you can expect high security and quality standards, while others enjoy the easy to navigate blogging dashboard for getting content out quickly and painlessly.</p>
+
+                <a class="continue-button" href="#">Continue Reading</a>
+            </article>
+        </div>
+        <div class="post-header">
+            <span class="date-box">26.09<br>2016</span>
+            <span class="post-title"><h4>Top 30 Best Free Blogger Templates</h4></span>
+            <span class="comment-box">23</span>
+        </div>
+        <div class="post-content">
+            <figure>
+                <img src="img/thumbnail.jpg">
+                <figcaption>Some Random Text About The Photo</figcaption>
+            </figure>
+
+            <article>
+                <p>Blogger is possibly the oldest blogging platform still actively running on the web, Google Blogger has been home to millions of bloggers for over 16 years now, and still maintains a quite stable market share in the content management platform
+                    list. Out of all the known websites that run a content management system, Blogger is used by 2.8% of those websites, which comes out at being used on 1.2% of all known websites to man; thanks W3Techs. Some stick with Blogger because
+                    it’s made by Google, and you can expect high security and quality standards, while others enjoy the easy to navigate blogging dashboard for getting content out quickly and painlessly.</p>
+                <p>Blogger is possibly the oldest blogging platform still actively running on the web, Google Blogger has been home to millions of bloggers for over 16 years now, and still maintains a quite stable market share in the content management platform
+                    list. Out of all the known websites that run a content management system, Blogger is used by 2.8% of those websites, which comes out at being used on 1.2% of all known websites to man; thanks W3Techs. Some stick with Blogger because
+                    it’s made by Google, and you can expect high security and quality standards, while others enjoy the easy to navigate blogging dashboard for getting content out quickly and painlessly.</p>
+                <p>Blogger is possibly the oldest blogging platform still actively running on the web, Google Blogger has been home to millions of bloggers for over 16 years now, and still maintains a quite stable market share in the content management platform
+                    list. Out of all the known websites that run a content management system, Blogger is used by 2.8% of those websites, which comes out at being used on 1.2% of all known websites to man; thanks W3Techs. Some stick with Blogger because
+                    it’s made by Google, and you can expect high security and quality standards, while others enjoy the easy to navigate blogging dashboard for getting content out quickly and painlessly.</p>
+            </article>
+        </div>
+    </div>
+</body>
+
+</html>
